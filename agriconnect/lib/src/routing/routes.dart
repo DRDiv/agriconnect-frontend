704a2808d4fa6aa176d23b75c493ad87a1873bed
Akshit517 <akshitmandial517@gmail.com>
2024-03-08 21:01:43 +0530

    basic register screen

--- agriconnect/lib/src/routing/routes.dart
+++ agriconnect/lib/src/routing/routes.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 
 import 'package:agriconnect/src/screens/login.dart';
+import 'package:agriconnect/src/screens/register.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-GoRouter goRouter = GoRouter(initialLocation: "/login", routes: [
+GoRouter goRouter = GoRouter(initialLocation: "/register", routes: [
   GoRoute(
     path: "/login",
     pageBuilder: (context, state) {
@@ -25,6 +26,29 @@ GoRouter goRouter = GoRouter(initialLocation: "/login", routes: [
         },
         transitionDuration: const Duration(milliseconds: 750),
         child: LoginScreen(),
+      );
+    },
+  ),
+  GoRoute(
+    path: "/register",
+    pageBuilder: (context, state) {
+      return CustomTransitionPage(
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0);
+          const end = Offset.zero;
+          const curve = Curves.ease;
+
+          var tween = Tween(begin: begin, end: end).chain(
+            CurveTween(curve: curve),
+          );
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+        transitionDuration: const Duration(milliseconds: 750),
+        child: RegisterScreen(),
       );
     },
   )
